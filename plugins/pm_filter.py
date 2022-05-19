91f4ffd13f7df6c18b2dbe421aaabb21337aec58
Shuhaib N C <56123591+ShuhaibNC@users.noreply.github.com>
2022-05-19 11:53:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -26,7 +26,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming & filters.private)
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
